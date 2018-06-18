e8465d8e87ca7f3575b4c855404543d2e6a54840
arpana kumari <arpanabitp1317@gmail.com>
2018-06-18 11:50:46 +0530

    first commit

--- multipledata.js
+++ multipledata.js
@@ -1,35 +1,38 @@
 var mongoose = require('mongoose');
-//make as connection
 
-mongoose.connect('mongodb://localhost:27017/tutorial');
-//get a reference to database
-var db = mongoose.Collection();
+// make a connection 
+mongoose.connect('mongodb://localhost:27017/tutorialkart');
 
-db.on('err', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('connection successful');
+// get reference to database
+var db = mongoose.connection;
 
-    // define Schema
-    var BookSchema = mongoose.Schema({
-        name: String,
-        price: Number,
-        quantity: Number
-    });
+db.on('error', console.error.bind(console, 'connection error:'));
 
-    // compile schema to model
-    var Book = mongoose.model('Book', BookSchema, 'bookstore');
+db.once('open', function() {
+	console.log("Connection Successful!");
+	
+	// define Schema
+	var BookSchema = mongoose.Schema({
+	  name: String,
+	  price: Number,
+	  quantity: Number
+	});
 
-    // documents array
-    var books = [{ name: 'Mongoose Tutorial', price: 10, quantity: 25 },
-    { name: 'NodeJS tutorial', price: 15, quantity: 5 },
-    { name: 'MongoDB Tutorial', price: 20, quantity: 2 }];
+	// compile schema to model
+	var Book = mongoose.model('Book', BookSchema, 'bookstore');
 
-    // save multiple documents to the collection referenced by Book Model
-    Book.collection.insert(books, function (err, docs) {
-        if (err) {
-            return console.error(err);
-        } else {
-            console.log("Multiple documents inserted to Collection");
-        }
-    });
+	// documents array
+	var books = [{ name: 'Mongoose Tutorial', price: 10, quantity: 25 },
+					{ name: 'NodeJS tutorial', price: 15, quantity: 5 },
+					{ name: 'MongoDB Tutorial', price: 20, quantity: 2 }];
+
+	// save multiple documents to the collection referenced by Book Model
+	Book.collection.insert(books, function (err, docs) {
+	  if (err){ 
+		  return console.error(err);
+	  } else {
+	    console.log("Multiple documents inserted to Collection");
+	  }
+	});
+	
 });
